73e697edb836929e31fbb5024a9a59149c84280f
Nick Shryock <n.shryock10@gmail.com>
2021-04-08 06:38:32 -0700

    JS error fix

---
+++ script.js
@@ -0,0 +1,33 @@
+var move = "100px";
+var sliderLimit = -750;
+
+
+//New JS test on 'About Me'
+let section1 = document.getElementById('about-me');
+let colorChange = function() {
+    section1.style.backgroundColor = 'green';
+};
+section1.onkeydown = colorChange;
+
+//Button color change test
+let btn = document.getElementById('button');
+
+let hoverChange = () => {
+    btn.style.backgroundColor = 'red';
+};
+btn.onmouseover = hoverChange;
+
+$("#right-arrow").click(function(){
+
+    var currentPosition = parseInt(view.css("left"));
+    if( currentPosition >= sliderLimit) view.stop(false, true).animate({left:"-="+move},{duration: 400});
+
+})
+
+$("#left-arrow").click(function(){
+
+    var currentPosition = parseInt(view.css("left"));
+    if( currentPosition < 0) view.stop(false, true).animate({left:"+="+move},{duration: 400});
+    $("#left-arrow").css('background-color', 'red');
+    
+})
