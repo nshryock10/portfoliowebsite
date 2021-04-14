036855c1d1d9c85d66ecb5540e3821439a05cf57
Nick Shryock <n.shryock10@gmail.com>
2021-04-14 05:21:49 -0700

    JavaScript files added and started

--- script.js
+++ script.js
@@ -1,33 +1,54 @@
-var move = "100px";
-var sliderLimit = -750;
+let elem = document.getElementById("button");
+let leftArrow = document.getElementById("left-arrow");
+let rightArrow = document.getElementById("right-arrow");
+let projA = document.getElementById("a");
+let projB = document.getElementById("b");
+let projC = document.getElementById("c");
+let projADisplay = getComputedStyle(projA).display;
+let projBDisplay = getComputedStyle(projB).display;
+let projCDisplay = getComputedStyle(projC).display;
 
+function clickChange() {
 
-//New JS test on 'About Me'
-let section1 = document.getElementById('about-me');
-let colorChange = function() {
-    section1.style.backgroundColor = 'green';
-};
-section1.onkeydown = colorChange;
+    elem.style.backgroundColor = 'red';
+}
 
-//Button color change test
-let btn = document.getElementById('button');
+function changeColor() {
+    event.target.style.backgroundColor =  'rgba(128, 128, 128, 0.55)';
+}
 
-let hoverChange = () => {
-    btn.style.backgroundColor = 'red';
-};
-btn.onmouseover = hoverChange;
+function changeBack() {
+    event.target.style.backgroundColor =  'rgba(128, 128, 128, 0.034)';
+}
+function toggleRight() {
+    if(projADisplay === "block"){
+        projA.style.display = 'none';
+        projB.style.display = 'block';
+    }else if(projBDisplay === "block"){
+        projB.style.display = 'none';
+        projC.style.display = 'block';
+    }else if(projCDisplay === "block"){
+        projC.style.display = 'none';
+        projA.style.display = 'block';
+    }
+}
+function toggleLeft() {
+    if(projADisplay === "block"){
+        projA.style.display = 'none';
+        projC.style.display = 'block';
+    }else if(projCDisplay === "block"){
+        projC.style.display = 'none';
+        projB.style.display = 'block';
+    }else if(projBDisplay === "block"){
+        projB.style.display = 'none';
+        projA.style.display = 'block';
+    }
+}
 
-$("#right-arrow").click(function(){
-
-    var currentPosition = parseInt(view.css("left"));
-    if( currentPosition >= sliderLimit) view.stop(false, true).animate({left:"-="+move},{duration: 400});
-
-})
-
-$("#left-arrow").click(function(){
-
-    var currentPosition = parseInt(view.css("left"));
-    if( currentPosition < 0) view.stop(false, true).animate({left:"+="+move},{duration: 400});
-    $("#left-arrow").css('background-color', 'red');
-    
-})
+elem.onclick = clickChange;
+leftArrow.onmouseover = changeColor;
+leftArrow.onmouseleave = changeBack;
+rightArrow.onmouseover = changeColor;
+rightArrow.onmouseleave = changeBack;
+rightArrow.onclick = toggleRight;
+leftArrow.onclick = toggleLeft;
