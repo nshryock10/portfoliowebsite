b32996c80eaf33477bd58f03ad29f7af59f03731
Nick Shryock <n.shryock10@gmail.com>
2021-04-21 06:52:47 -0700

    Final HTML and initialstyling

--- script.js
+++ script.js
@@ -7,11 +7,9 @@ let projC = document.getElementById("c");
 let projADisplay = getComputedStyle(projA).display;
 let projBDisplay = getComputedStyle(projB).display;
 let projCDisplay = getComputedStyle(projC).display;
+let btn = document.getElementById("button");
+let formBtn = document.getElementById("form-btn")
 
-function clickChange() {
-
-    elem.style.backgroundColor = 'red';
-}
 
 function changeColor() {
     event.target.style.backgroundColor =  'rgba(128, 128, 128, 0.55)';
@@ -21,6 +19,9 @@ function changeBack() {
     event.target.style.backgroundColor =  'rgba(128, 128, 128, 0.034)';
 }
 function toggleRight() {
+    let projADisplay = getComputedStyle(projA).display;
+    let projBDisplay = getComputedStyle(projB).display;
+    let projCDisplay = getComputedStyle(projC).display;
     if(projADisplay === "block"){
         projA.style.display = 'none';
         projB.style.display = 'block';
@@ -33,6 +34,9 @@ function toggleRight() {
     }
 }
 function toggleLeft() {
+    let projADisplay = getComputedStyle(projA).display;
+    let projBDisplay = getComputedStyle(projB).display;
+    let projCDisplay = getComputedStyle(projC).display;
     if(projADisplay === "block"){
         projA.style.display = 'none';
         projC.style.display = 'block';
@@ -44,8 +48,21 @@ function toggleLeft() {
         projA.style.display = 'block';
     }
 }
+function btnHover() {
+    event.target.style.backgroundColor = 'black';
+    event.target.style.color = 'white';
+    event.target.style.border = 'border: 3px solid gray';
+}
+function btnBack() {
+    event.target.style.backgroundColor = 'white';
+    event.target.style.color = 'black';
+    event.target.style.border = '3px solid rgba(0, 0, 0, 0.808);';
+}
 
-elem.onclick = clickChange;
+btn.onmouseover = btnHover;
+btn.onmouseleave = btnBack;
+formBtn.onmouseover = btnHover;
+formBtn.onmouseleave = btnBack;
 leftArrow.onmouseover = changeColor;
 leftArrow.onmouseleave = changeBack;
 rightArrow.onmouseover = changeColor;
